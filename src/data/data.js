11bba7b12a1709898800cbfbfc90b2e4191b1c95
Galekwan Sango <galekwansango@gmail.com>
2024-01-02 00:32:25 -0500

    images path updated

--- src/data/data.js
+++ src/data/data.js
@@ -3,7 +3,7 @@ export const projects = [
     Name: 'E-store',
     Description:
       'E-store is a full stack project developed by a team of five engineers that mimics the implementation of an online retailer shop.In the e-store, users can browser, custom and order items they want.The project was class assignment that had the goal introducing us to the cores principles of Software Engineer, such as:  Development life Cycle, Requirements Engineering, Software Design, Project Elaboration and Planning , testing and more ',
-    image: '../../public/images/e-store.png',
+    image: '/images/e-store.png',
     id: 1,
     icons: ['FaAngular', 'SiTypescript', 'FaHtml5', 'FaCss3'],
     size: 40,
@@ -12,7 +12,7 @@ export const projects = [
     Name: 'Src-ML',
     Description:
       'srcML is a free software application licensed under GPL. it a lightweight, highly scalable, robust, multi-language parsing tool to convert source code into srcML that offers an infrastructure for the exploration, analysis, and manipulation of source code which are transformed in XML format.  ',
-    image: '../../public/images/srcML-logo.svg',
+    image: '/images/srcML-logo.svg',
     id: 2,
     icons: ['FaReact', 'FaNodeJs', 'FaHtml5', 'FaCss3', 'IoLogoJavascript'],
     size: 40,
@@ -21,7 +21,7 @@ export const projects = [
     Name: 'Tenzy ',
     Description:
       'Fast, fun, engaging, and surprisingly intricate, Tenzi is perfect for table top beginners and aficionados alike. the player gets 10 dice. The object of the game is to roll the dice as fast as possible, the player who gets all dice on the same number and yells "Tenzi!" is the winner. the purpose of this project was to build a fun dynamic game that require user interaction',
-    image: '../../public/images/Tenzi-game.jpeg',
+    image: '/images/Tenzi-game.jpeg',
     id: 3,
     icons: ['FaReact', 'FaHtml5', 'FaCss3', 'IoLogoJavascript'],
     size: 40,
@@ -30,7 +30,7 @@ export const projects = [
     Name: 'Digital card ',
     Description:
       'Professional digital card that provide a short summary of who I am and my interests. The purpose of this project was to build a simple static react page ',
-    image: '../../public/images/digital-card.png',
+    image: '/images/digital-card.png',
     id: 4,
     icons: ['FaReact', 'FaHtml5', 'FaCss3', 'IoLogoJavascript'],
     size: 40,
@@ -39,7 +39,7 @@ export const projects = [
     Name: 'Trivia Quiz ',
     Description:
       'Quizzicle Trivia Quiz game is a free game full of fun that consists of guessing the answers for variety of fun questions. Quizzicle Trivia Quiz includes different kinds of games, from traditional quizzes to brainteaser style fast games. Plenty of photo quizzes of fun facts, celebrities riddles and more. Crack the trivia and answer the riddles.',
-    image: '../../public/images/quizzical-game.jpeg',
+    image: '/images/quizzical-game.jpeg',
     id: 5,
     icons: ['FaReact', 'FaHtml5', 'FaCss3', 'IoLogoJavascript'],
     size: 40,
